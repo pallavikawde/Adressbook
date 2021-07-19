a2a9e7f1bb30307425b75dd18d9370ded5f310c5
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-19 11:46:06 +0530

    Added:UC1_created constructer

--- src/com/timbhuchalka/AdressB.java
+++ src/com/timbhuchalka/AdressB.java
@@ -1,10 +1,34 @@
 package com.timbhuchalka;
 
-public class AdressB {
-    public class Adress {
+class Contact{
+
+      private String firstname;
+      private String lastname;
+      private String adress;
+      private String city;
+      private String state;
+      private String zip;
+      private String ponenumber;
+      private String email;
+
+    public Contact(String firstname, String lastname, String adress, String city, String state, String zip, String ponenumber, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.adress = adress;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.ponenumber = ponenumber;
+        this.email = email;
+    }
+}
+
+
+
+        public class AdressB {
+
 
         public static void main(String[] args) {
             System.out.println("welcome to addressbook ");
         }
     }
-}
